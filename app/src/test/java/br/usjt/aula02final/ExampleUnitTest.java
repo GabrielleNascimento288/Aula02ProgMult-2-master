498fb44fe28c5371087234c465b7a4b78c333ad6
Gabrielle_Nascimento <gabriellenascimento288@gmail.com>
2018-03-21 23:22:17 -0300

    Initial commit

---
+++ app/src/test/java/br/usjt/aula02final/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package br.usjt.aula02final;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Gabrielle Nascimento da Anunciação - 81612332 - SIN3AN-MCA
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
